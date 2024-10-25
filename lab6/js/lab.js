a5b3543f2ed828534942e96866f007e212d7a3ef
chimchimchimmy <Stephanielipe1@gmail.com>
2024-10-24 23:58:06 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Stephanie Lipe
+// Date: October 24, 2024
 
 // Constants
 
@@ -19,3 +19,22 @@ function main() {
 
 // let's get this party started
 main();
+
+// Define Variables
+myTransport = ["car", "bus", "walk", "other peoples' cars"];
+
+// create an object for my main ride
+myMainRide = {
+  make: "Kia",
+  model: "Forte",
+  color: "White",
+  year: "2019",
+  age: function() {
+      return 2024 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transport I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
